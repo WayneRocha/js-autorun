f85fe1f8917f32779b07fedbf3c91785aa92ecf5
Wayne Rocha <dev.waynerocha@gmail.com>
2022-04-29 11:55:57 -0300

    <built> dinamic UI

--- index.js
+++ index.js
@@ -1,19 +1,79 @@
-(() => {
-    const scriptsList = {
-        "scriptsList": [
-            {
-                whereToExecute: 'https://github.com/WayneRocha/js-autorun',
-                content: 'console.log("running script")'
-            },
-            {
-                whereToExecute: 'https://context.reverso.net/traducao/portugues-espanhol/programador',
-                content: `const blockedElements = document.querySelectorAll('.blocked');const bannerElement = document.querySelector('#blocked-results-banner');blockedElements.forEach(element => element.classList.remove('blocked'));bannerElement.remove();`
-            }
-        ]
-    };
-
-    chrome.storage.sync.set(scriptsList);
-    //renderScripts(scriptsList.scriptsList, document.getElementById('scripts'));
-})();
-
 hljs.highlightAll();
+const scriptsList = [
+    {
+        "id": Math.random().toString().replace(".", ""),
+        "run": true,
+        "name": "Welcome to JS Autorun",
+        "content": "console.log('JS Autorun is running');",
+        "where": ["https://google.com/", "chrome://extensions/"],
+        "timeOut": 0
+    },
+    {
+        "id": Math.random().toString().replace(".", ""),
+        "run": true,
+        "name": "Hello world",
+        "content": "console.log('hello wolrd');",
+        "where": ["https://github.com/waynerocha/", "chrome://extensions/"],
+        "timeOut": 3
+    }
+];
+function setScripts(scripts){
+    chrome.storage.sync.set({"scriptsList": scriptsList});
+}
+
+function runInCurrentTab({where}){
+    return true;
+    return where.some(url => {
+        const anyMatch = [
+            window.location.href === url,
+            window.location.host === url,
+            window.location.hostname === url,
+            window.location.origin === url
+        ].some(urlMatch => urlMatch );
+        return anyMatch;
+    });
+}
+
+function renderScriptsList(scriptsList){
+    scriptsList.forEach(script => {
+        document.querySelector('.script-items-list').innerHTML += `
+        <li class="menu-item" data-script-id="${script.id}">
+        <p>${script.name}</p>
+        <div class="switch"></div>
+        </li>
+        `;
+        if (runInCurrentTab(script)){
+            document.querySelector('.menu-items-list').innerHTML += `
+            <li class="menu-item" data-script-id="${script.id}">
+                <p>${script.name}</p>
+                <div class="switch"></div>
+            </li>
+            `
+        }
+    });
+
+    const $menuItems = [...document.querySelectorAll('.menu-item')];
+    $menuItems.forEach($item => {
+        $item.addEventListener('click', (e) => {
+            $menuItems.forEach($item => $item.classList.remove("active"));
+            e.target.classList.add("active");
+            const scriptId = e.target.getAttribute("data-script-id");
+            renderScriptDetails(scriptId);
+        });
+    });
+    renderScriptDetails(scriptsList[0].id);
+}
+
+function renderScriptDetails(scriptId){
+    chrome.storage.sync.get(["scriptsList"], (result) => {
+        const { name, content, where, timeOut } = result.scriptsList.find(script => script.id === scriptId);
+        document.querySelector("body > main > div > h1").textContent = name;
+        document.querySelector("pre code").innerHTML = content;
+        document.querySelector("body > main > div > input[type=text]").value = where[0];
+        document.querySelector("#dispatch-after-seconds").value = timeOut;
+        hljs.highlightAll();
+    });
+}
+
+setScripts(scriptsList);
+renderScriptsList(scriptsList);
--- runner.js
+++ runner.js
@@ -1,10 +1,20 @@
-(async () => {
-  return;
-  function runInCurrentTab({whereToExecute}){
-    return window.location.href === whereToExecute
+(() => {
+  return 0;
+  function runInCurrentTab({ where }) {
+    return true;
+    return where.some(url => {
+      return (
+        window.location.href === url ||
+        window.location.host === url ||
+        window.location.hostname === url ||
+        window.location.origin === url
+      );
+    });
   }
-  
-  function runScript(script){
+
+  function runScript(script) {
+    console.log('running');
+    console.log(script);
     const $scriptDispatcherElement = document.createElement('p');
     $scriptDispatcherElement.style.display = "none";
     $scriptDispatcherElement.setAttribute('onclick', `javascript:(() => {${script}})();`);
@@ -13,27 +23,31 @@
   }
 
   function getScriptList() {
-    return new Promise(resolve => chrome.storage.sync.get(["scriptsList"], scripts => resolve(scripts.scriptList)));
+    return new Promise(resolve => chrome.storage.sync.get(["scriptsList"], (result) => resolve(result.scriptsList)));
   }
 
   function getScript() {
     return new Promise(async(resolve, reject) => {
       try {
         const scripts = await getScriptList();
-        scripts.scriptsList.forEach(script => {
-          if (runInCurrentTab(script.whereToExecute)) {
+        console.log(JSON.stringify(scripts));
+        scripts.scriptList.forEach(script => {
+          if (runInCurrentTab(script.where)) {
             resolve(script.content);
           }
         });
       } catch (error) {
         reject(error);
-      } finally {
-        resolve([]);
       }
     });
   }
+  console.log(getScript);
 
   getScript
-    .then(script => { if (script) runScript(script); })
+  .then((script) => {
+      if (script){
+        setTimeout(runScript.bind(script), script.timeOut * 1000);
+      }
+    })
     .catch((error) => console.error(error));
 })();
